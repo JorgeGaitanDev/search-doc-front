ced13810e25a6a4ac93e3567236a1d099919ec18
JorgeGaitanDev <jgaitanh12@gmail.com>
2024-05-25 02:09:14 -0500

    updated

--- src/app/components/buscar-documento/buscar-documento.component.ts
+++ src/app/components/buscar-documento/buscar-documento.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { DocumentoService } from 'src/app/services/documento.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-buscar-documento',
@@ -11,13 +13,33 @@ import { DocumentoService } from 'src/app/services/documento.service';
   styleUrls: ['./buscar-documento.component.css']
 })
 export class BuscarDocumentoComponent implements OnInit {
+
+    usuario: {
+      firstName: string,
+      secondName: string,
+      firstSurname: string,
+      secondSurname: string,
+      phone: number,
+      address: string,
+      cityResident: string
+    } = {
+      firstName: '',
+      secondName: '',
+      firstSurname: '',
+      secondSurname: '',
+      phone: 0,
+      address: '',
+      cityResident: ''
+    };
+
   documentoForm: FormGroup;
   constructor(private fb: FormBuilder,
               private router: Router,
               private toastr: ToastrService,
               private _documentoService: DocumentoService,
-              private aRouter: ActivatedRoute) {
-    this.documentoForm = this.fb.group({
+              private aRouter: ActivatedRoute,
+              private http: HttpClient) {
+  this.documentoForm = this.fb.group({
       tipo_documento: ['', Validators.required],
       num_documento:  ['', [Validators.required, Validators.minLength(8), Validators.maxLength(11), Validators.pattern('^[0-9]*$')]]
     })
@@ -26,19 +48,23 @@ export class BuscarDocumentoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  agregarDocumento() {
+  agregarDocumento(): void {
     if (this.documentoForm.valid) {
-    console.log(this.documentoForm)
-    const DOCUMENTO: Documento = {
-      tipo_documento: this.documentoForm.get('tipo_documento')?.value,
-      num_documento: this.documentoForm.get('num_documento')?.value,
-    }
-    console.log(DOCUMENTO);
-    this.toastr.info('Se ha encontrado la información básica', 'Búsqueda Exitosa!');
-    this.router.navigate(['/info-documento']);
-  } else {
-    this.documentoForm.markAllAsTouched(); // Marcar todos los controles como tocados para activar mensajes de validación
+      const tipo_documento = this.documentoForm.get('tipo_documento')?.value;
+      const num_documento = this.documentoForm.get('num_documento')?.value;
 
-  }
+      // Construir la URL con los valores del formulario
+      const url = `http://localhost:8090/user_information/identification_type/${tipo_documento}/identification/${num_documento}`;
+
+      // Hacer la petición GET a la URL construida
+      this.http.get<any>(url).subscribe((response: any) => {
+        this.usuario = response;
+        this.router.navigate(['/info-documento']);
+      }, (error: any) => {
+        console.error(error);
+      });
+    } else {
+      this.documentoForm.markAllAsTouched();
+    }
   }
 }
--- src/app/services/documento.service.ts
+++ src/app/services/documento.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Documento } from '../models/documento';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentoService {
+  url = 'http://localhost:8090/user_information/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Método para obtener información del usuario
+  agregarDocumento(identificationType: string, userId: string): Observable<any> {
+    const userUrl = `${this.url}identification_type/${identificationType}/identification/${userId}`;
+    return this.http.get(userUrl);
+  }
 }
